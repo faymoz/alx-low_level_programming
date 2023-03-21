29211966f208bb55d46045baa55f63bf10a78447
faymoz <faymoz007@gmail.com>
2023-03-21 04:59:34 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,14 @@
-	
+#include <stdio.h>
+/**
+ * main - A program that prints the size of various types on the computer
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf("size of char: %zu bytes\n", sizeof(char));
+printf("size of char: %zu bytes\n", sizeof(int));
+printf("size of char: %zu bytes\n", sizeof(long int));
+printf("size of char: %zu bytes\n", sizeof(long long int));
+printf("size of char: %zu bytes\n", sizeof(float));
+return (0);
+}
