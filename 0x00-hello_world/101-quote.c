ee631a0b22e5648b4e245af8225e5142940f8aff
faymoz <faymoz007@gmail.com>
2023-03-22 04:26:40 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,6 @@
  */
 int main(void)
 {
-const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, message, sizeof(message) - 1);
-return 1;
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"stdout);
+return (1);
 }
