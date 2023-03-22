a28f2c61dc2c48ba95c21df1170a9b42af7f0e4c
faymoz <faymoz007@gmail.com>
2023-03-21 21:10:21 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-printf("Size of char: %zu byte(s)\n", sizeof(char));
-printf("Size of int: %zu byte(s)\n", sizeof(int));
-printf("Size of long int: %zu byte(s)\n", sizeof(long));
-printf("Size of long long int: %zu byte(s)\n", sizeof(long long));
-printf("Size of float: %zu byte(s)\n", sizeof(float));
+printf("Size of char: %lu byte(%)\n", sizeof(char));
+printf("Size of int: %lu byte(%)\n", sizeof(int));
+printf("Size of long int: %lu byte(%)\n", sizeof(long int));
+printf("Size of long long int: %lu byte(%)\n", sizeof(long long int));
+printf("Size of float: %lu byte(%)\n", sizeo(float));
 return (0);
 }
